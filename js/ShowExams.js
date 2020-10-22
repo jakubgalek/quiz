73f8b295c4660f7be1ecc3cd1adfb7622e62144d
Wondun <49043385+Wondun@users.noreply.github.com>
2020-10-22 19:06:10 +0200

    click area

--- js/ShowExams.js
+++ js/ShowExams.js
@@ -1,16 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const titleOfExam = document.querySelectorAll(".titleOfExam");
+    const Exam = document.querySelectorAll(".exam");
     const titleOfExamTest = document.querySelectorAll(".titleOfExamTest");
 
 
-    for (let i = 0; i < titleOfExam.length; i++) {
-        titleOfExam[i].addEventListener('click', showExams = i => {
+    for (let i = 0; i < Exam.length; i++) {
+        Exam[i].addEventListener('click', showExams = i => {
             for (let j = 0; j < titleOfExamTest.length; j++) {
                 titleOfExamTest[j].style.display = "block";
             }
-        }
-
-        );
+        });
     }
 
 });
