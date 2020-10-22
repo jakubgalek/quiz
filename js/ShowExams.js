85cd1b5d6c99fcf79a4e81a36b4a6e92bddc68a6
Wondun <49043385+Wondun@users.noreply.github.com>
2020-10-22 19:01:23 +0200

    arrow function

--- js/ShowExams.js
+++ js/ShowExams.js
@@ -2,14 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const titleOfExam = document.querySelectorAll(".titleOfExam");
     const titleOfExamTest = document.querySelectorAll(".titleOfExamTest");
 
-    const showExams = i => {
-        for (let j = 0; j < titleOfExamTest.length; j++) {
-            titleOfExamTest[0].style.display = "block";
-            titleOfExamTest[1].style.display = "block";
-        }
-    }
+
     for (let i = 0; i < titleOfExam.length; i++) {
-        titleOfExam[i].addEventListener('click', showExams(i));
+        titleOfExam[i].addEventListener('click', showExams = i => {
+            for (let j = 0; j < titleOfExamTest.length; j++) {
+                titleOfExamTest[j].style.display = "block";
+            }
+        }
+
+        );
     }
 
 });
