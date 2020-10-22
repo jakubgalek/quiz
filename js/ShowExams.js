4538ae4c91196864b02f375ae0c68691e1a9a3a9
Wondun <49043385+Wondun@users.noreply.github.com>
2020-10-22 19:06:55 +0200

    div area Exam

--- js/ShowExams.js
+++ js/ShowExams.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const Exam = document.querySelectorAll(".exam");
-    const titleOfExamTest = document.querySelectorAll(".titleOfExamTest");
+    const titlesOfExamTest = document.querySelectorAll(".titleOfExamTest");
 
 
     for (let i = 0; i < Exam.length; i++) {
         Exam[i].addEventListener('click', showExams = i => {
-            for (let j = 0; j < titleOfExamTest.length; j++) {
-                titleOfExamTest[j].style.display = "block";
+            for (let j = 0; j < titlesOfExamTest.length; j++) {
+                titlesOfExamTest[j].style.display = "block";
             }
         });
     }
