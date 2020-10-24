42a8b2c6a1bb663b4bf6bfc9e2486a6b8761350f
Wondun <49043385+Wondun@users.noreply.github.com>
2020-10-24 22:38:03 +0200

    Searching script

---
+++ js/Search.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", _ => {
+
+    const search = document.querySelector(".search");
+    const searching = () => {
+
+        let input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById('myInput');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myUL");
+        li = ul.getElementsByClassName('.titleOfExamTest');
+
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            txtValue = a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+
+    search.addEventListener('keyup', searching);
+
+});
