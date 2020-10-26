61d438bfc04525882478756e4624f3496f38ab07
Wondun <49043385+Wondun@users.noreply.github.com>
2020-10-26 14:28:29 +0100

    fixed list

--- js/ShowExams.js
+++ js/ShowExams.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const exams = document.querySelectorAll(".exam");
-
     exams.forEach(exam => {
-        const title = document.querySelector('.titleOfExam')
-        const test = document.querySelector('.titleOfExamTest')
+        const title = exam.querySelector('.titleOfExam')
+        const testList = exam.querySelector('.exam-tests-list')
+
         title.addEventListener("click", () => {
-            title.classList.toggle('active')
+            testList.classList.toggle('active')
         });
     });
-    exams.forEach(exam => {
-        const title = document.querySelector('.titleOfExam')
-        const test = document.querySelector('.titleOfExamTest')
-        title.addEventListener("click", () => {
-            title.classList.toggle('active')
-        });
+    title.addEventListener("click", () => {
+        testList.classList.toggle('active')
     });
 });
